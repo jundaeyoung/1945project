2e682240510ca3d1cef1e07f6e8d80456c85fb4b
seoyounglee0105 <os010312@naver.com>
2023-02-23 15:06:19 +0900

    하트3개 사라지기까지 구현

--- src/project/EnemyBullet.java
+++ src/project/EnemyBullet.java
@@ -3,104 +3,113 @@ package project;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
 public class EnemyBullet extends JLabel implements Moveable {
 
-	private int x;
-	private int y;
+   private int x;
+   private int y;
 
-	private boolean down;
+   private boolean down;
 
-	private int state;
-	private ImageIcon enemyBullet;
-	private ImageIcon boom;
-	private ImageIcon gost;
+   private int state;
+   private int attackCount;
 
-	private AirplaneFrame mContext;
-	private BackgroundEnemyBulletService backgroundenemyBulletService;
+   private ImageIcon enemyBullet;
+   private ImageIcon boom;
+   private ImageIcon player;
 
-	public EnemyBullet(AirplaneFrame mContext) {
-		this.mContext = mContext;
-		initData();
-		setInitLayout();
-		backgroundenemyBulletService = new BackgroundEnemyBulletService(this);
-		initThread();
-	}
+   private AirplaneFrame mContext;
+   private BackgroundEnemyBulletService backgroundenemyBulletService;
 
-	public void initData() {
-		enemyBullet = new ImageIcon("imagesProject/bullet4.png");
-		boom = new ImageIcon("imagesProject/explosion.gif");
-		state = 0;
+   public EnemyBullet(AirplaneFrame mContext) {
+      this.mContext = mContext;
+      initData();
+      setInitLayout();
+      backgroundenemyBulletService = new BackgroundEnemyBulletService(this);
+      initThread();
+   }
 
-	}
+   public void initData() {
+      enemyBullet = new ImageIcon("imagesProject/bullet4.png");
+      boom = new ImageIcon("imagesProject/explosion.gif");
+      player = new ImageIcon("imagesProject/BigPlane2.png");
+      state = 0;
+      // 1로 계속 초기화
+      attackCount = 1;
+      mContext.getPlayer().setIcon(player);
 
-	public void setInitLayout() {
-		x = mContext.getEnemy().getX() + 30;
-		y = mContext.getEnemy().getY() + 50;
-		setIcon(enemyBullet);
-		setSize(100, 100);
-		setLocation(x, y);
-	}
+   }
 
-	public void initThread() {
-		new Thread(new Runnable() {
-			public void run() {
-				down();
-			}
-		}).start();
-	}
+   public void setInitLayout() {
+      x = mContext.getEnemy().getX() + 30;
+      y = mContext.getEnemy().getY() + 50;
+      setIcon(enemyBullet);
+      setSize(100, 100);
+      setLocation(x, y);
+   }
 
-	public void crash() {
-		Life life = new Life(mContext);
-		Player player = new Player(mContext);
-		try {
-			Thread.sleep(100);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		player.beAttack();
-		mContext.repaint();
-		System.out.println("추락합니다.");
-		if (mContext.getPlayer().getLife() == 0) {
-			setIcon(boom);
-			state = 1;
-			mContext.remove(mContext.getPlayer());
-			mContext.repaint();
-		}
-	}
+   public void initThread() {
+      new Thread(new Runnable() {
+         public void run() {
+            down();
+         }
+      }).start();
+   }
 
-	@Override
-	public void left() {
+   public void crash() {
+//      Player player = new Player(mContext);
 
-	}
+      if (attackCount == 1) {
+         attackCount--;
+         mContext.getPlayer().beAttack();
+         mContext.repaint();
 
-	@Override
-	public void right() {
+         if (mContext.getPlayer().getLife() == 0) {
+            setIcon(boom);
+            state = 1;
+            mContext.remove(mContext.getPlayer());
+            mContext.repaint();
+         }
+      }
 
-	}
+   }
 
-	@Override
-	public void up() {
+   @Override
+   public void left() {
 
-	}
+   }
 
-	@Override
-	public void down() {
-		down = true;
-		while (true) {
-			y++;
-			setLocation(x, y);
-			if (Math.abs(x - mContext.getPlayer().getX()) < 10 && Math.abs(y - mContext.getPlayer().getY()) < 50) {
-				if (mContext.getPlayer().getStatus() == 0) {
-					crash();
-				}
-			}
-			try {
-				Thread.sleep(3);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
+   @Override
+   public void right() {
+
+   }
+
+   @Override
+   public void up() {
+
+   }
+
+   @Override
+   public void down() {
+      down = true;
+      while (true) {
+         y++;
+         setLocation(x, y);
+         if (Math.abs(x - mContext.getPlayer().getX()) < 10 && Math.abs(y - mContext.getPlayer().getY()) < 50) {
+            if (mContext.getPlayer().getStatus() == 0) {
+               crash();
+            }
+         }
+         try {
+            Thread.sleep(3);
+         } catch (InterruptedException e) {
+            e.printStackTrace();
+
+         }
+
+      }
+   }
 
 }
--- src/project/Player.java
+++ src/project/Player.java
@@ -5,305 +5,311 @@ import javax.swing.JLabel;
 
 public class Player extends JLabel implements Moveable {
 
-	PlayerWay playerWay;
-	AirplaneFrame mContext;
+   PlayerWay playerWay;
+   AirplaneFrame mContext;
 
-	private int x;
-	private int y;
-	private int bulletX;
-	private int bulletY;
-	private int status;
-	private ImageIcon player;
-	private ImageIcon bullet;
-	private ImageIcon gost;
-	private int life = 3;
+   private int x;
+   private int y;
+   private int bulletX;
+   private int bulletY;
+   private int status;
+   private ImageIcon player;
+   private ImageIcon bullet;
+   private ImageIcon gost;
+   private int life = 3;
 
-	// 플레이어의 현재 움직임 상태 기록
-	private boolean left;
-	private boolean right;
-	private boolean up;
-	private boolean down;
-	private boolean attack;
+   // 플레이어의 현재 움직임 상태 기록
+   private boolean left;
+   private boolean right;
+   private boolean up;
+   private boolean down;
+   private boolean attack;
 
-	// 벽에 충돌한 상태
-	private boolean leftWallCrash;
-	private boolean rightWallCrash;
-	private boolean upWallCrash;
-	private boolean downWallCrash;
+   // 벽에 충돌한 상태
+   private boolean leftWallCrash;
+   private boolean rightWallCrash;
+   private boolean upWallCrash;
+   private boolean downWallCrash;
 
-	// 플레이어의 속도
-	private final int SPEED = 4;
+   // 플레이어의 속도
+   private final int SPEED = 4;
 
-	public int getLife() {
-		return life;
-	}
+   public int getLife() {
+      return life;
+   }
 
-	public void setLife(int life) {
-		life = life;
-	}
+   public void setLife(int life) {
+      life = life;
+   }
 
-	public int getStatus() {
-		return status;
-	}
+   public int getStatus() {
+      return status;
+   }
 
-	public void setStatus(int status) {
-		this.status = status;
-	}
+   public void setStatus(int status) {
+      this.status = status;
+   }
 
-	public AirplaneFrame getmContext() {
-		return mContext;
-	}
+   public AirplaneFrame getmContext() {
+      return mContext;
+   }
 
-	public void setmContext(AirplaneFrame mContext) {
-		this.mContext = mContext;
-	}
+   public void setmContext(AirplaneFrame mContext) {
+      this.mContext = mContext;
+   }
 
-	public int getX() {
-		return x;
-	}
+   public int getX() {
+      return x;
+   }
 
-	public void setX(int x) {
-		this.x = x;
-	}
+   public void setX(int x) {
+      this.x = x;
+   }
 
-	public int getY() {
-		return y;
-	}
+   public int getY() {
+      return y;
+   }
 
-	public void setY(int y) {
-		this.y = y;
-	}
+   public void setY(int y) {
+      this.y = y;
+   }
 
-	public ImageIcon getPlayer() {
-		return player;
-	}
+   public ImageIcon getPlayer() {
+      return player;
+   }
 
-	public void setPlayer(ImageIcon player) {
-		this.player = player;
-	}
+   public void setPlayer(ImageIcon player) {
+      this.player = player;
+   }
 
-	public ImageIcon getBullet() {
-		return bullet;
-	}
+   public ImageIcon getBullet() {
+      return bullet;
+   }
 
-	public void setBullet(ImageIcon bullet) {
-		this.bullet = bullet;
-	}
+   public void setBullet(ImageIcon bullet) {
+      this.bullet = bullet;
+   }
 
-	public boolean isLeft() {
-		return left;
-	}
+   public boolean isLeft() {
+      return left;
+   }
 
-	public void setLeft(boolean left) {
-		this.left = left;
-	}
+   public void setLeft(boolean left) {
+      this.left = left;
+   }
 
-	public boolean isRight() {
-		return right;
-	}
+   public boolean isRight() {
+      return right;
+   }
 
-	public void setRight(boolean right) {
-		this.right = right;
-	}
+   public void setRight(boolean right) {
+      this.right = right;
+   }
 
-	public boolean isUp() {
-		return up;
-	}
+   public boolean isUp() {
+      return up;
+   }
 
-	public void setUp(boolean up) {
-		this.up = up;
-	}
+   public void setUp(boolean up) {
+      this.up = up;
+   }
 
-	public boolean isDown() {
-		return down;
-	}
+   public boolean isDown() {
+      return down;
+   }
 
-	public void setDown(boolean down) {
-		this.down = down;
-	}
+   public void setDown(boolean down) {
+      this.down = down;
+   }
 
-	public boolean isLeftWallCrash() {
-		return leftWallCrash;
-	}
+   public boolean isLeftWallCrash() {
+      return leftWallCrash;
+   }
 
-	public void setLeftWallCrash(boolean leftWallCrash) {
-		this.leftWallCrash = leftWallCrash;
-	}
+   public void setLeftWallCrash(boolean leftWallCrash) {
+      this.leftWallCrash = leftWallCrash;
+   }
 
-	public boolean isRightWallCrash() {
-		return rightWallCrash;
-	}
+   public boolean isRightWallCrash() {
+      return rightWallCrash;
+   }
 
-	public void setRightWallCrash(boolean rightWallCrash) {
-		this.rightWallCrash = rightWallCrash;
-	}
+   public void setRightWallCrash(boolean rightWallCrash) {
+      this.rightWallCrash = rightWallCrash;
+   }
 
-	public boolean isUpWallCrash() {
-		return upWallCrash;
-	}
+   public boolean isUpWallCrash() {
+      return upWallCrash;
+   }
 
-	public void setUpWallCrash(boolean upWallCrash) {
-		this.upWallCrash = upWallCrash;
-	}
+   public void setUpWallCrash(boolean upWallCrash) {
+      this.upWallCrash = upWallCrash;
+   }
 
-	public boolean isDownWallCrash() {
-		return downWallCrash;
-	}
+   public boolean isDownWallCrash() {
+      return downWallCrash;
+   }
 
-	public void setDownWallCrash(boolean downWallCrash) {
-		this.downWallCrash = downWallCrash;
-	}
+   public void setDownWallCrash(boolean downWallCrash) {
+      this.downWallCrash = downWallCrash;
+   }
 
-	public int getSPEED() {
-		return SPEED;
-	}
+   public int getSPEED() {
+      return SPEED;
+   }
 
-	public Player(AirplaneFrame mContext) {
-		this.mContext = mContext;
+   public Player(AirplaneFrame mContext) {
+      this.mContext = mContext;
 
-		initData();
-		setInitLayout();
-	}
+      initData();
+      setInitLayout();
+   }
 
-	public void initData() {
-		player = new ImageIcon("imagesProject/BigPlane2.png");
-		gost = new ImageIcon("imagesProject/PLANE2무적.png");
-		playerWay = PlayerWay.RIGHT;
-	}
+   public void initData() {
+      player = new ImageIcon("imagesProject/BigPlane2.png");
+      gost = new ImageIcon("imagesProject/PLANE2무적.png");
+      playerWay = PlayerWay.RIGHT;
+   }
 
-	public void setInitLayout() {
-		x = 470;
-		y = 805;
-		setSize(70, 70);
-		setLocation(x, y);
-		setIcon(player);
-	}
+   public void setInitLayout() {
+      x = 470;
+      y = 805;
+      setSize(70, 70);
+      setLocation(x, y);
+      setIcon(player);
+   }
 
-	@Override
-	public void left() {
-		left = true;
-		playerWay = PlayerWay.LEFT;
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (left) {
-					x -= SPEED;
-					setLocation(x, y);
+   @Override
+   public void left() {
+      left = true;
+      playerWay  = PlayerWay.LEFT;
+      new Thread(new Runnable() {
+         @Override
+         public void run() {
+            while (left) {
+               x -= SPEED;
+               setLocation(x, y);
 
-					try {
-						Thread.sleep(15);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+               try {
+                  Thread.sleep(15);
+               } catch (InterruptedException e) {
+                  // TODO Auto-generated catch block
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
+   }
 
-	@Override
-	public void right() {
-		playerWay = PlayerWay.RIGHT;
-		right = true;
+   @Override
+   public void right() {
+      playerWay  = PlayerWay.RIGHT;
+      right = true;
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (right) {
-					x += SPEED;
-					setLocation(x, y);
+      new Thread(new Runnable() {
+         @Override
+         public void run() {
+            while (right) {
+               x += SPEED;
+               setLocation(x, y);
 
-					try {
-						Thread.sleep(15);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+               try {
+                  Thread.sleep(15);
+               } catch (InterruptedException e) {
+                  // TODO Auto-generated catch block
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
+   }
 
-	@Override
-	public void up() {
-		playerWay = PlayerWay.UP;
-		up = true;
+   @Override
+   public void up() {
+      playerWay  = PlayerWay.UP;
+      up = true;
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (up) {
-					y -= SPEED;
-					setLocation(x, y);
+      new Thread(new Runnable() {
+         @Override
+         public void run() {
+            while (up) {
+               y -= SPEED;
+               setLocation(x, y);
 
-					try {
-						Thread.sleep(15);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
+               try {
+                  Thread.sleep(15);
+               } catch (InterruptedException e) {
+                  // TODO Auto-generated catch block
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
 
-	}
+   }
 
-	@Override
-	public void down() {
-		playerWay = PlayerWay.DOWN;
-		down = true;
+   @Override
+   public void down() {
+      playerWay  = PlayerWay.DOWN;
+      down = true;
 
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (down) {
-					y += SPEED;
-					setLocation(x, y);
+      new Thread(new Runnable() {
+         @Override
+         public void run() {
+            while (down) {
+               y += SPEED;
+               setLocation(x, y);
 
-					try {
-						Thread.sleep(15);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+               try {
+                  Thread.sleep(15);
+               } catch (InterruptedException e) {
+                  // TODO Auto-generated catch block
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
+   }
 
-	public void space() {
-		new Thread(new Runnable() {
-			@Override
-			public void run() {
-				while (attack) {
-					System.out.println("space");
-					setIcon(bullet);
-					bulletX += 10;
-					setLocation(bulletX, bulletY);
-					try {
-						Thread.sleep(3);
-					} catch (InterruptedException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		}).start();
-	}
+   public void space() {
+      new Thread(new Runnable() {
+         @Override
+         public void run() {
+            while (attack) {
+               System.out.println("space");
+               setIcon(bullet);
+               bulletX += 10;
+               setLocation(bulletX, bulletY);
+               try {
+                  Thread.sleep(3);
+               } catch (InterruptedException e) {
+                  e.printStackTrace();
+               }
+            }
+         }
+      }).start();
+   }
 
-	public void beAttack() {
-		life = life - 1;
-		if (life == 2) {
-			mContext.remove(mContext.getLife2());
-		} else if (life == 1) {
-			mContext.remove(mContext.getLife1());
-		} else if (life == 0) {
-			mContext.remove(mContext.getLife0());
-			setIcon(gost);
-		}
-	}
+   public void beAttack() {
+      life = life-1;
+      System.out.println("추락합니다.");
+      if (life == 2) {
+         mContext.getLife2().setIcon(null);
+         setIcon(gost);
+   
+      } else if (life == 1) {
+         mContext.getLife1().setIcon(null);
+         System.out.println("2번째 추락");
+         setIcon(gost);
+      } else {
+         mContext.getLife0().setIcon(null);
+         System.out.println("3번째 추락");
+         setIcon(gost);
+      }
+   }
 
-	public void attack() {
-		Bullet bullet = new Bullet(mContext);
-		mContext.add(bullet);
-	}
+   public void attack() {
+      Bullet bullet = new Bullet(mContext);
+      mContext.add(bullet);
+   }
 }
