36fdba3da8a6ed8ff795f3cf05bf5a9c92d1ccb5
jiun0516 <114144833+jiun0516@users.noreply.github.com>
2023-02-24 17:15:12 +0900

    enemy unit 생성

---
+++ src/project/EnemyUnit1.java
@@ -0,0 +1,47 @@
+package project;
+
+import javax.swing.ImageIcon;
+
+public class EnemyUnit1 extends Enemy2 {
+
+	EnemyTestFrame2 mContext;
+
+	public EnemyUnit1(EnemyTestFrame2 mContext) {
+		this.mContext = mContext;
+		initData();
+		setInitLayout();
+	}
+
+	public void initData() {
+		hp = 4;
+		speed = 1;
+		alive = 0;
+		enemyImage = new ImageIcon("imagesProject/enemy2.png");
+	}
+
+	private void setInitLayout() {
+		setSize(500, 500);
+		setIcon(enemyImage);
+	}
+
+	@Override
+	public void down(int speed) {
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				down = true;
+				while (alive == 0) {
+					attack();
+					y += speed;
+					setLocation(x, y);
+					try {
+						Thread.sleep(30);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		}).start();
+	}
+
+}
